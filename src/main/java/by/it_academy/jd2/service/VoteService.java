dd14e76ebc67e57230cb1a93aaad6a7e327bfe91
Ilya Test <westdragon0@gmail.com>
2024-08-05 20:58:06 +0300

    использование хранилище, добавление валидации

--- src/main/java/by/it_academy/jd2/service/VoteService.java
+++ src/main/java/by/it_academy/jd2/service/VoteService.java
@@ -2,26 +2,23 @@ package by.it_academy.jd2.service;
 
 import by.it_academy.jd2.dto.VoteDTO;
 import by.it_academy.jd2.service.api.IVoteService;
+import by.it_academy.jd2.storage.api.IVoteStorage;
 
-import java.util.HashMap;
-import java.util.Map;
 
 public class VoteService implements IVoteService {
 
     private final static VoteService instance = new VoteService();
-    private Map<String, Integer> artist = new HashMap<>();
+    private final static IVoteStorage voteStorage = VoteStorageFile.getInstance();
 
     private VoteService() {
     }
 
     @Override
     public void create(VoteDTO vote) {
-        artist.compute(vote.getArtist(), (k,v) ->{
-            if(v == null){
-                return 1;
-            }
-            return v + 1;
-        });
+        if(vote.getArtist() == null || vote.getArtist().isBlank()){
+            throw new IllegalArgumentException("Артист пуст");
+        }
+        voteStorage.create(vote);
     }
 
     public static VoteService getInstance(){
