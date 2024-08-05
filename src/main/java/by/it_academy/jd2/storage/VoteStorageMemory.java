6f3cdea3e5e527a739f544a36b432071ced7dc3a
Ilya Test <westdragon0@gmail.com>
2024-08-05 20:57:14 +0300

    добавление отдельного слоя хранилища

---
+++ src/main/java/by/it_academy/jd2/storage/VoteStorageMemory.java
@@ -0,0 +1,43 @@
+package by.it_academy.jd2.storage;
+
+import by.it_academy.jd2.dto.VoteDTO;
+import by.it_academy.jd2.storage.api.IVoteStorage;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class VoteStorageMemory implements IVoteStorage {
+
+    private final static VoteStorageMemory instance = new VoteStorageMemory();
+    private Map<String, Integer> artist = new HashMap<>();
+    private Map<String, Integer> genre = new HashMap<>();
+    private List<String> abouts = new ArrayList<>();
+
+    private VoteStorageMemory() {
+    }
+
+    @Override
+    public void create(VoteDTO vote) {
+        inc(artist, vote.getArtist());
+
+        for (String genre : vote.getGenre()) {
+            inc(this.genre, genre);
+        }
+        abouts.add(vote.getAbout());
+    }
+
+    private void inc(Map<String, Integer> data, String key){
+        data.compute(key, (k,v) ->{
+            if(v == null){
+                return 1;
+            }
+            return v + 1;
+        });
+    }
+
+    public static VoteStorageMemory getInstance() {
+        return instance;
+    }
+}
---
+++ src/main/java/by/it_academy/jd2/storage/api/IVoteStorage.java
@@ -0,0 +1,7 @@
+package by.it_academy.jd2.storage.api;
+
+import by.it_academy.jd2.dto.VoteDTO;
+
+public interface IVoteStorage {
+    void create(VoteDTO vote);
+}
